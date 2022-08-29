3674ad8eae1277b5f3389a3f43f8e0330ba1e9ea
catherinebarrientos <barrientoscb05@gmail.com>
2022-08-29 21:07:23 +0800

    complete 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArr, ...args) {    
+    args.forEach(element => {
+        let index = mainArr.indexOf(element);
+        if(index > -1)
+            mainArr.splice(index, 1);
+    });
 
+    return mainArr;
 };
 
 // Do not edit below this line
