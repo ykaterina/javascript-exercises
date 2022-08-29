6d50b441d8b314d2484c92125b295c4712f29070
catherinebarrientos <barrientoscb05@gmail.com>
2022-08-29 20:34:53 +0800

    complete 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, noOfRepititions) {
+    let textToDisplay = "";
+    if(noOfRepititions > 0){
+        for(let i = 0; i < noOfRepititions; i++)
+            textToDisplay += text;
+    } else {
+        textToDisplay = 'ERROR';
+    }
+    return textToDisplay;   
 };
 
 // Do not edit below this line
