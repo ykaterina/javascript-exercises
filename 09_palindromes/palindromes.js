aa678550d5e5c24eb4597c4593330a32c29cf88c
catherinebarrientos <barrientoscb05@gmail.com>
2022-10-12 18:01:00 +0800

    complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let half = Math.trunc(input.length / 2);
 
+    input = input.replace(/[^a-z]/gi, '').toLowerCase();
+
+    let isPalindrome = true;
+    let j = input.length - 1;
+    for(let i = 0; i < half; i++){
+        if(input.charAt(i) !== input.charAt(j)){
+            isPalindrome = false;
+            break;
+        }
+        j--;
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
